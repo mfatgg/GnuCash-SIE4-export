9d9dcad95b9b7a3341397498028ea341f98d05b3
Jonas <i@gizur.com>
2022-10-01 11:26:36 +0200

    Första fugnerande version

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# GnuCash SIE4 export
+# GnuCash SIE4 export (i Python3)
 
 Skapa SIE4-export (https://sie.se) från GnuCash. CSV-filer med transaktioner, resultat och balans skapas också.
 NOTE: SIE4 innehåller endast transaktionerna och *inte* ingående eller utgående balanser eller någon annan info.
 
+1. Spara GnuCash bokföring i sqlite format, t.ex. `ftgabc.sqlit`
+2. Kopiera `company_header.se` till `ftgabc_header.se`
+3. Öppna `ftgabc_header.se` i en text editor och uppdatera (fälten borde vara självförklarande)
+4. Kör python scriptet: `python3 reports.py ftgabc.sqlite ftgabc 2022` (Kör scriptet utan argument så visas en enkel hjälp: `python3 reports.py`)
 
+SIE-filen har testats i Bokio och kan importeras där, men några omfattande tester har inte gjorts. All feedback är välkommen, skapa en issue
+här i Github om du stöter på problem.
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# GnuCash SIE4 export
+# Playground for GnuCash reports on top of sqlite db
 #
 # Resources
 # * https://wiki.gnucash.org/wiki/SQL
@@ -16,8 +16,13 @@ tables = ['accounts',          'customers',         'lots',              'splits
 import sqlite3
 import argparse
 import pandas as pd
+import sys
 
-DB_PATH = '../sqlitedb'
+from pathlib import Path
+from os.path import exists
+
+
+# DB_PATH = '../sqlitedb'
 DB_EXT  = 'gnucash'
 
 EXCLUDE_TRANS = []
@@ -29,18 +34,26 @@ pd.set_option('display.expand_frame_repr', True)
 pd.options.display.float_format = '{:,.2f}'.format
 
 parser = argparse.ArgumentParser(description='Balans och resultat.')
-parser.add_argument(dest='year', type=ascii,
+parser.add_argument(dest='dbfile', type=ascii,
                     action='store',
-                    help='År')
+                    help='Database file to use')
 parser.add_argument(dest='company', type=ascii,
                     action='store',
-                    help='Företag')
+                    help='Database file to use')
+parser.add_argument(dest='year', type=ascii,
+                    action='store',
+                    help='Database file to use')
 args = parser.parse_args()
 
+DB_FILE = args.dbfile.strip("'")
 COMPANY = args.company.strip("'")
 YEAR = args.year.strip("'")
+#DB_FILE = f'{DB_PATH}/{YEAR}/{COMPANY}_{YEAR}.{DB_EXT}'
 
-DB_FILE = f'{DB_PATH}/{YEAR}/{COMPANY}_{YEAR}.{DB_EXT}'
+if not exists(DB_FILE):
+    sys.exit(f'ERROR: {DB_FILE} does not exits')
+
+
 print(f'Report based on: {DB_FILE}')
 
 
@@ -76,7 +89,13 @@ dfb = dft[['code','value']].groupby(['code']).sum().reset_index()
 print(dfb.columns)
 print(dfb.head(100))
 dfb.to_csv(f'{COMPANY}_{YEAR}-balans.csv', index=False)
-print(f'Diff:{dfb["value"].sum()}')
+
+ek = dfb[dfb["code"]<2000].sum()[1]
+tillg = dfb[(dfb["code"]>=2000) & (dfb["code"]<3000)].sum()[1]
+
+print(f'EK:\t\t\t{ek:,.2f}\nTillgångar:\t\t{tillg:,.2f}\nBeräknat resultat:\t{ek+tillg:,.2f}')
+print(f'')
+
 
 print('\n************************** Resultatkonton ***********************************')
 dft = df[df['code']>2999]
@@ -85,10 +104,18 @@ df['value'] = pd.to_numeric(df['value'])
 print(dfr.columns)
 print(dfr.head(100))
 dfr.to_csv(f'{COMPANY}_{YEAR}-resultat.csv', index=False)
-print(f'Beräknat resultat:{dfr["value"].sum()} (<0 innebär vinst)')
+print(f'Beräknat resultat:{dfr["value"].sum():,.2f} (<0 innebär vinst)')
 
 
-printnum = -1
+print('\n************************** SIE (alpha - experiment) ***********************************')
+
+print('\nDropping rows without account code\n', df[df['code'].isnull()])
+df = df[df['code'].notnull()]
+
+df['code'] = df['code'].astype(int)
+df.post_date = df.post_date.str.replace('-','')
+df.enter_date = df.enter_date.str.replace('-','')
+prev_num = -1
 first_trans = True
 res = ''
 df = df.sort_values('num')
@@ -100,15 +127,20 @@ for index, row in df.iterrows():
 
         res = res + '#VER A {} {} \"{}\" {}\n{{\n'.format(
                     row['num'],
-                    row['post_date'][0:10],
+                    row['post_date'][0:8],
                     row['description'][0],
-                    row['enter_date'][0:10])
+                    row['enter_date'][0:8])
     res = res + '#TRANS {} {{}} {:.2f} \"\" \"\" 0\n'.format(
             row['code'],
             row['value'])
     prev_num = row['num']
 
 res = res + '}\n'
+
+# Add SIE header to file
+header = Path(f'{COMPANY}_header.se').read_text()
+print('\nAdding SIE header to SIE file\n', header)
+res = header + res
 
 file = open(f'{COMPANY}_{YEAR}.se', 'w')
 file.write(res)
